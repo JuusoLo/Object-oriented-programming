e57a02376cdb8f4e833ab77fe97533ee754b5775
JuusoLo <lonka.juuso@gmail.com>
2018-01-11 11:03:45 +0200

    TaskPerson Example done

---
+++ object-method/TestPerson/TestPerson/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TaskPerson
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Person personRuger = new Person();
+            personRuger.Name = "Ruger";
+            personRuger.GrowOld();
+            personRuger.PrintPersonInfo();
+
+            Person newPerson = new Person("Joe Doe", 17);
+            newPerson.PrintPersonInfo();
+            newPerson.GrowOld();
+            newPerson.PrintPersonInfo();
+            Console.ReadKey();
+
+        }
+    }
+}
