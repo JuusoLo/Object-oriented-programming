525706d4998bdf3c18badd5827fc1cbdb983bbed
JuusoLo <lonka.juuso@gmail.com>
2018-01-12 09:39:01 +0200

    Task Book Done

---
+++ object-method/Task_Book/Task_Book/Book.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_Book
+{
+    class Book
+    {
+        //Kirjan tiedot
+        public string Title;
+        public string Author;
+        public int Id;
+        public double Price;
+
+        //Constructor
+        public Book()
+        {
+            Title = "Legend of Skrt";
+            Author = "Olli Jokinen";
+            Id = 9965119;
+            Price = 29.99;
+
+        }
+        public Book(string title, int id, string author, double price)
+        {
+            Title = title;
+            Author = author;
+            Id = id;
+            Price = price;
+
+        }
+        //Methods
+
+        public void PrintBookInfo()
+        {
+            Console.WriteLine($"Kirjan nimi: {Title}\n" +
+                $"Kirjoittajan nimi: {Author}\n" +
+                $"Id-numero: {Id}\n" +
+                $"Kappalehinta: {Price}\n" +
+                $"------------------------------------");
+
+        }
+
+        public string CompareBook(Book book)
+        {
+            if (this.Price > book.Price)
+                return ($"{this.Title} on kalliimpi kuin {book.Title} kirja\n");
+
+            else
+                return ($"{book.Title} on kalliimpi kuin {this.Title} kirja\n");
+
+
+
+        }
+
+    }
+}
---
+++ object-method/Task_Book/Task_Book/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task_Book
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Book newBook = new Book("Legend of Skrt", 9965119,"Olli Jokinen", 29.99);
+            newBook.PrintBookInfo();
+
+            Book newBook2 = new Book("Karhukopla", 88487716, "Urho Kakkonen", 39.99);
+            newBook2.PrintBookInfo();
+
+            Book newBook3 = new Book("Ralli Mikko", 88998727, "Migis Turbbis", 19.99);
+            newBook3.PrintBookInfo();
+
+            Console.WriteLine($"{newBook2.CompareBook(newBook)}");
+            Console.WriteLine($"{newBook3.CompareBook(newBook)}");
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
