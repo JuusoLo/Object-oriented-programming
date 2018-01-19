c28afd954906c4a59f6ac23b3700f125087c32cf
JuusoLo <lonka.juuso@gmail.com>
2018-01-19 11:27:34 +0200

    Task.Ticket Ready

---
+++ object-method/Task_Ticket/Task_Ticket/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task_Ticket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Ticket newticket = new Ticket();
+            newticket.AskPersonInfo();
+            newticket.ShowPersonInfo();
+            newticket.Discount();
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
