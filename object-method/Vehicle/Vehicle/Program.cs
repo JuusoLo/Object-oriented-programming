a7466425f662bd269d95457d8065fe705a6cf7fc
JuusoLo <lonka.juuso@gmail.com>
2018-02-01 10:20:58 +0200

    Task.Ajoneuvo kesken

---
+++ object-method/Vehicle/Vehicle/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Vehicle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Hei tervetuloa! Autojen tiedot:");
+            Vehicle newVehicle1 = new Vehicle("Farmari","BMW","2015",39990);
+            newVehicle1.PrintCarInfo();
+
+            Vehicle newVehicle2 = new Vehicle("Sedan", "Toyota", "2011", 9700);
+            newVehicle2.PrintCarInfo();
+
+
+            Console.ReadKey();
+            
+    
+        }
+    }
+}
---
+++ object-method/Vehicle/Vehicle/Vehicle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vehicle
+{
+    class Vehicle
+    {
+        public string Type;
+        public string Brand;
+        public string Model;
+        public double Price;
+
+
+        //Constructor
+
+        public Vehicle(string type, string brand, string model, double price)
+        {
+            Type = type;
+            Brand = brand;
+            Model = model;
+            Price = price;
+        }
+
+        public Vehicle()
+        {
+            Type = "Ei tiedossa";
+            Brand = "Ei tiedossa";
+            Model = "Ei tiedossa";
+            Price = 0;
+        }
+
+        public void PrintCarInfo()
+        {
+            Console.WriteLine($"Auton malli: {Type}\n" +
+                $"Merkki: {Brand}\n" +
+                $"Vuosimalli: {Model}\n" +
+                $"Hinta: {Price:c}\n" +
+                $"---------------------------");
+        }
+
+
+
+        
+    }
+}
