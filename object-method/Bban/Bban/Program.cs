3ff2db5b8340a1c54219e37287833cee75f50d03
JuusoLo <lonka.juuso@gmail.com>
2018-02-16 10:19:54 +0200

    Task.Bban

---
+++ object-method/Bban/Bban/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Bban
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            string machineFormat = "227720-35988";
+            bool isMachineFormat = UtilityBban.Correctnumber(ref machineFormat);
+            if (isMachineFormat)
+            {
+                Console.WriteLine($"{machineFormat} - {UtilityBban.IsValidAccount(machineFormat)}");
+            }
+            else
+            {
+                Console.WriteLine("Account number incorrect!");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ object-method/Bban/Bban/bban.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bban
+{
+    class bban
+    {
+    }
+}
