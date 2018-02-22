59de6d30d14f205caec887a71268e5e73af24b0e
JuusoLo <lonka.juuso@gmail.com>
2018-02-22 10:59:10 +0200

    Rajapinta kesken.

---
+++ object-method/Rajapinta/Rajapinta/ITuote.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rajapinta
+{
+    interface ITuote
+    {
+        //method
+        void HaeTuote();
+        void LaskeBonus();
+    }
+}
