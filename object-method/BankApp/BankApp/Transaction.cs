3f0a5ddfae2152add6ee7ff07cb54a8395cc8994
JuusoLo <lonka.juuso@gmail.com>
2018-03-23 11:37:32 +0200

    BankApp update

---
+++ object-method/BankApp/BankApp/Transaction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankApp
+{
+    class Transaction
+    {
+        private double _sum;
+        public DateTime _timeStamp;
+
+        public double Sum { get => _sum; set => _sum = value; }
+
+        public Transaction(double sum, DateTime timeStamp)
+        {
+            _sum = sum;
+            _timeStamp = timeStamp;
+        }
+    }
+}
